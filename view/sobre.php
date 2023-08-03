e8b60825595c188e5784b4a9e53b301d766afdf9
Arthur <arthurafonsoifpr@gmail.com>
2023-08-03 08:53:45 -0300

    front-end

--- view/sobre.php
+++ view/sobre.php
@@ -8,35 +8,90 @@ include(__DIR__ . "/componentes/sideBar.php");
 <head>
     <title>Sobre o IF</title>
     <link rel="stylesheet" href="./css/ingresso.css">
-</head>
-
-<body>
-<div>
-            <h1>Sobre o IFPR</h1>
-            <p>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>  
                 O IFPR se destaca por ser uma instituição pública de ensino que oferece 
                 formação gratuita aos estudantes. Além disso, também é conhecido por explorar o 
                 aprendizado prático com a vivência de experiências baseadas na realidade de 
                 atuação profissional das diferentes áreas.
             </p>
-            <p>Os Institutos Federais são instituições, pluricurriculares e multicampi 
+            <p>Os Institutos Federais são instituições pluricurriculares e multicampi 
                 (reitoria, campus, campus avançado, polos de inovação e polos de educação a 
                 distância), especializados na oferta de educação profissional e tecnológica 
                 (EPT) em todos os seus níveis e formas de articulação com os demais níveis e 
-                modalidades da Educação Nacional, oferta os diferentes tipos de cursos de EPT, 
+                modalidades da Educação Nacional, oferta diferentes tipos de cursos de EPT, 
                 além de licenciaturas, bacharelados e pós-graduação stricto sensu. Instituídos no 
                 momento de constituição da Rede Federal, os institutos têm como obrigatoriedade legal 
                 garantir um mínimo de 50% de suas vagas para a oferta de cursos técnicos de nível 
-                médio, prioritariamente na forma integrada. Devem ainda, garantir o mínimo 
+                médio, prioritariamente na forma integrada. Devem ainda garantir o mínimo 
                 de 20% de suas vagas para atender a oferta de cursos de licenciatura, bem
                 como programas especiais de formação pedagógica, com vistas a formação de 
                 professores para a educação básica, sobretudo nas áreas de ciências e 
                 matemática, e para a educação profissional. Destaca-se também sua atribuição 
                 no desenvolvimento de soluções técnicas e tecnológicas por meio de pesquisas
                 aplicadas e as ações de extensão junto à comunidade com vistas ao avanço 
-                econômico e social local e regional.
-            </p>
-        </div>
+                econômico e social local e regional.</p>
+
+                <br> <h2>Objetivo da Instituição</h2>
+<p><br> Promover a educação profissional e tecnológica, pública e de qualidade através do ensino, pesquisa e extensão, com o objetivo de formar cidadãos críticos, autônomos, empreendedores e comprometidos com a sustentabilidade.
+
+<br>Valores:
+
+<br>• Pessoas;
+<br>• Educação de qualidade e excelência;
+<br>• Eficiência e eficácia;
+<br>• Ética;
+<br>• Sustentabilidade;
+<br>• Qualidade de vida;
+<br>• Diversidade humana e cultural;
+<br>• Inclusão social;
+<br>• Empreendedorismo e inovação;
+<br>• Democracia e transparência.
+
+</p>
+
+<br><h2>Corpo Docente</h2>
+<p>
+
+<div class="card" style="width: 18rem;">
+  <img src="odacir-zanatta.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Odacir Antonio Zanatta</h5>
+    <p class="card-text">Reitor</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Fone: (41) 3888-5253</li>
+    <li class="list-group-item"><a href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do;jsessionid=5588CF164D673A762A648B6E0297E906.buscatextual_0">Currículo</a></li>
+    <li class="list-group-item"><a href="https://eagendas.cgu.gov.br/?_token=eaZG0viyEepFDECqeKvXLxVPirWWYhVoE7TkZln0&filtro_orgao=1188&filtro_cargo=REITOR&filtro_servidor=6203#divcalendar">Agenda</a></li>
+  </ul>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="nelson-neto.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Nelson de Castro Neto</h5>
+    <p class="card-text">Diretor Executivo</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Fone: (41) 3888-5267</li>
+    <li class="list-group-item"><a href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do">Currículo</a></li>
+    <li class="list-group-item"><a href="https://ifpr.edu.br/acesso-a-informacao/institucional/agenda-dos-gestores/agendas-da-reitoria/#dex">Agenda</a></li>
+  </ul>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="anderson-coldbella.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Anderson Coldebella</h5>
+    <p class="card-text">Diretor Executivo</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><a href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do">Currículo</a></li>
+  </ul>
+</div>
+   </p></div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
