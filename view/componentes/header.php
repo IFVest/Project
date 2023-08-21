d2f07fb775cf5d21557d70d81ef7bdec22085ffd
thiagochan <thiagoortega.ifpr@gmail.com>
2023-08-21 10:34:42 -0300

    Alterando a tela de relatório

--- view/componentes/header.php
+++ view/componentes/header.php
@@ -142,7 +142,7 @@ $isTeacher = $acessService->hasRole(UserRoles::Professor);
                     </div>
                 </div>
             </div>
-            <script src=" https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.4/cjs/popper.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     </body>
--- view/exam/test_exam.php
+++ view/exam/test_exam.php
@@ -2,6 +2,7 @@
     include(__DIR__ . "/../componentes/header.php");
     require_once(__DIR__ . "/../../util/config.php");
     require_once(__DIR__ . "/../../controller/ExamController.php");
+    require_once(__DIR__.'/examTestCreator.php')
 ?>
 
 <!DOCTYPE html>
@@ -19,52 +20,13 @@
             <h1 class="mt-3">Simulado</h1>
             <form method="POST" action="<?= BASE_URL ?>/controller/ExamController.php?action=makeReport">
                 <?php
-                $alternatives = ['a', 'b', 'c', 'd', 'e'];
                 $exam = $dados['prova'];
                 $examModules = $exam->getExamModules();
-                $questionCount = 1;
-                $alternativeCounter = 0;
+
+                ExamTestCreator::mapTest($exam, $examModules);
+                
                 ?>
-                <div class="<?= $exam->getId()?>">
-                <?php foreach($examModules as $exMod):?>
-                    <div class="<?= $exMod->getId(); ?>">
-                        <?php foreach($exMod->getUserAnswers() as $userAnswer):
-                            $colorCircle = 'white';
-                            if($exam->getFinished()){
-                                $colorCircle = ($userAnswer->getUserRightAnswer())? 'green' : 'red';
-                            }
-                        ?>
-                            <div class="question container justify-content-center align-items-center d-flex flex-column bg-dark rounded mt-3 p-5" style="--bs-bg-opacity: .16;">
-                                <div class="p-3 mb-4 col-12 align-items-center d-flex">
-                                    <div class="d-flex align-items-center justify-content-center m-2 col-1"
-                                        style="height: 34px; width: 34px; border: 3px solid <?= $colorCircle; ?>; border-radius: 50%;">
-                                        <span><?= $questionCount; ?></span>
-                                    </div>
-                                    <span class="col-11"><?= $userAnswer->getQuestion()->getText(); ?></span>
-                                </div>
-                                <div class="col-12 d-flex flex-column align-items-center justify-center">
-                                    <?php foreach($userAnswer->getQuestion()->getAlternatives() as $alt): ?>
-                                        <input <?= ($exam->getFinished() && !$alt->getIsCorrect())? 'disabled' : '' ;?>
-                                                type="radio"
-                                                class="btn-check"
-                                                name="<?= $userAnswer->getId();?>" value="<?= $alt->getId();?>"
-                                                id="<?= $alt->getId();?>"
-                                                <?= ($userAnswer->getChosenAnswer() == $alt->getId())? 'checked' : '' ?>>
-                                        <label class="btn d-flex col-10 flex-direction-line
-                                                <?= ($exam->getFinished() && $alt->getIsCorrect())? 'btn-success' : '' ;?>
-                                                <?= ($exam->getFinished() && !$alt->getIsCorrect() && $userAnswer->getChosenAnswer() == $alt->getId())? 'btn-danger' : '' ;?> "
-                                                for="<?= $alt->getId();?>">
-                                            <?= $alternatives[$alternativeCounter].')  '. $alt->getText();?>
-                                        </label>
-                                        <?php $alternativeCounter++; ?>
-                                    <?php endforeach; $questionCount += 1; ?>
-                                </div>
-                            </div>
-                        <?php
-                        $alternativeCounter = 0;
-                        endforeach;?>
-                    </div>
-                <?php endforeach; ?>
+                
 
                 <input type="hidden" name="id" value="<?= $exam->getId()?>">
 
